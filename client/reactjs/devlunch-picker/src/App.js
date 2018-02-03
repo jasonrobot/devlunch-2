46905a8b7b3135857123e99475d753d4c81436a3
Jason Howell <jasonrobot64@gmail.com>
2018-02-03 13:56:33 -0800

    main UI is pretty well mocked in react

--- client/reactjs/devlunch-picker/src/App.js
+++ client/reactjs/devlunch-picker/src/App.js
@@ -1,18 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+/**
+ * data-deps:
+ * - app-state
+ */
+class Header extends Component {
   render() {
     return (
+      <div class="header">
+        {/* should say different things depending on app state */}
+        <div class="header_label">Time left: </div>
+        <Timer />
+      </div>
+    );
+  }
+}
+
+/**
+ * data-deps:
+ * - app-state: time of next state change
+ */
+class Timer extends Component {
+  render() {
+    return (
+      <div class="timer">
+        {/* TODO get data from app state or something */}
+        unknown
+      </div>
+    )
+  }
+}
+
+/**
+ * data-deps:
+ * - callbacks to update state
+ */
+class SignupForm extends Component {
+  render() {
+    return (
+      <div class="signup-form">
+        <input class="signup-form_nickname" placeholder="nickname"/>
+        <input class="signup-form_pick" placeholder="pick"/>
+        <button class="signup-form_minus">-</button>
+        <button class="signup-form_tilde">~</button>
+        <button class="signup-form_plus">+</button>
+      </div>
+    )
+  }
+}
+
+/**
+ * data-deps:
+ * - value to display
+ */
+class UserList extends Component {
+  render() {
+    return (
+      <div class="user-list">
+        {this.props.what}
+        <ul>
+          {this.props.users.map( (user) => {
+            return <li class="user-list_user">{user.nickname}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const NOT_COMING = 0,
+      JOINING = 1,
+      VOTING = 2;
+
+function makeUser(nickname, status) {
+  return {
+    nickname: nickname,
+    status: status
+  }
+}
+
+function getSampleUsers() {
+  return [
+    makeUser("foo", VOTING),
+    makeUser("bar", VOTING),
+    makeUser("baz", VOTING),
+    makeUser("usr", JOINING),
+    makeUser("etc", JOINING),
+    makeUser("tmp", JOINING),
+    makeUser("home", NOT_COMING),
+    makeUser("root", NOT_COMING)
+  ];
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allUsers: getSampleUsers()
+    }
+  }
+
+  render() {
+    let voting = this.state.allUsers.filter( (user) => {
+      return user.status === VOTING
+    });
+    let joining = this.state.allUsers.filter( (user) => {
+      return user.status === JOINING
+    });
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <SignupForm />
+        <UserList what="voting" users={voting} />
+        <UserList what="joining" users={joining}/>
       </div>
     );
   }
